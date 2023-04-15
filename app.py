e7041559370530198629da98ca0f90e9995eb525
Aaron Lockhart <aaronlockhartdev@gmail.com>
2023-04-14 22:00:43 -0400

    Added API lifespan to app actuator

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from fastapi.staticfiles import StaticFiles
 
 import api
 
-app = FastAPI(docs_url=None, redoc_url=None)
+app = FastAPI(docs_url=None, redoc_url=None, lifespan=api.lifespan)
 
 app.mount("/api/v1", api.app)
 app.mount("/", StaticFiles(directory=env["STATIC_DIR"], html=True))
